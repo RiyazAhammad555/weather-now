a43272e79fffadd76bbcbac5657d4dd978ab1891
RiyazAhammad555 <riyazahammad950@gmail.com>
2024-11-07 17:57:05 +0530

    project commit

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react";
+import axios from "axios";
+import WeatherForm from "./components/WeatherForm";
+import WeatherCard from "./components/WeatherCard";
+import ErrorMessage from "./components/ErrorMessage";
+
+function App() {
+  const [weatherData, setWeatherData] = useState([]);
+  const [locationData, setLocationData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading,setIsLoading]=useState(false)
+  const [selectedCity,setSelectedCity]=useState('')
+  const [type,setType]=useState('current')
+
+  const getWeather =(city) => {
+    setSelectedCity(city)
+    setError(null);
+    setIsLoading(true)
+    setType('current')
+    const getWeatherData=async()=>{
+      try {
+        
+        const geocodeResponse = await axios.get(
+          `http://api.weatherapi.com/v1/current.json?key=e668e9b859374576bb765451240711&q=${city}`
+        );
+        setWeatherData(geocodeResponse.data.current);
+        setLocationData(geocodeResponse.data.location)
+        setIsLoading(false) 
+      }catch (err) {
+        setError("Failed to fetch weather data. Please try again.");
+      }
+    }
+    setTimeout(getWeatherData,1000)
+    };
+
+  const getForecast=()=>{
+    setError(null);
+    setIsLoading(true)
+    setType('forecast')
+    const getForecastData=async()=>{
+      try {
+        const forecastResponse = await axios.get(
+        `http://api.weatherapi.com/v1/marine.json?key=e668e9b859374576bb765451240711&q=${selectedCity}&days=7`
+        );
+        console.log('g',forecastResponse)
+        setWeatherData(forecastResponse.data.forecast);
+        setLocationData(forecastResponse.data.location)
+        setIsLoading(false) 
+      }catch (err) {
+        setError("Failed to fetch weather data. Please try again.");
+      }
+    }
+    setTimeout(getForecastData,1000)
+  }
+
+  const getCurrentWeather=()=>{
+    getWeather(selectedCity)
+  }
+
+  return (
+    <div className="min-h-screen bg-blue-100 flex-col justify-center items-center">
+      <div class="navbar bg-base-100">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl">Hello,Jaime</a>
+        </div>
+        <div class="flex-none gap-2">
+          <div class="form-control">
+            {/* <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" /> */}
+          </div>
+          <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+              <div class="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+            <ul
+              tabindex="0"
+              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              <li>
+                <a class="justify-between">
+                  Profile
+                  <span class="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="w-full max-w-md p-6  m-auto mt-10">
+        <h1 className="text-3xl font-bold text-center text-blue-600 mb-4">
+          Weather Now
+        </h1>
+        <WeatherForm onSearch={getWeather} />
+        {error && <ErrorMessage message={error} />}
+      </div>
+      { Object.keys(weatherData).length>0  && !isLoading&&
+
+        <>
+          <div className="flex justify-self-center items-center mb-2">
+            <button type="btn" className="btn btn-primary w-40 ml-2" onClick={getCurrentWeather}>
+              Current
+            </button>
+            <button type="submit" className="btn btn-warning w-40 ml-2" onClick={getForecast}>
+              Forecast
+            </button>
+          </div>
+          <WeatherCard data={weatherData} locationData={locationData} type={type} />
+        </> 
+      }
+      {
+        isLoading && 
+        <div className="justify-self-center mt-auto">
+        <span className="loading loading-dots loading-lg text-black mr-2"></span>
+        <span className="loading loading-dots loading-lg text-black mr-2"></span>
+        <span className="loading loading-dots loading-lg text-black mr-2"></span>
+        <span className="loading loading-dots loading-lg text-black mr-2"></span>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { FaWind, FaTint, FaCloud, FaSun, FaMoon } from 'react-icons/fa';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    LineElement,
+    PointElement,  
+    CategoryScale,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+
+ChartJS.register(LineElement, PointElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
+
+const WeatherCard = ({ data, locationData, type }) => {
+    console.log('loc data', locationData)
+    const isDay = data.is_day;
+    const cardBackground = isDay ? "bg-blue-500" : "bg-gray-800";
+    const textColor = isDay ? "text-blue-900" : "text-white";
+    const iconColor = isDay ? "text-yellow-300" : "text-gray-200";
+
+    let chartData, chartOptions;
+    if (type === 'forecast') {
+        chartData = {
+            labels: type === 'forecast' ? data.forecastday.map(day => day.date) : [],  //x-axis
+            datasets: [
+                {
+                    label: 'Avg Temperature (°C)',
+                    data: type === 'forecast' ? data.forecastday.map(day => day.day.avgtemp_c) : [],  //Avg temperature for each day
+                    borderColor: '#FFD700',
+                    backgroundColor: 'rgba(255, 215, 0, 0.2)',
+                    fill: true,
+                    tension: 0.4,
+                },
+            ],
+        };
+
+        chartOptions = {
+            responsive: true,
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Date',
+                    },
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Temperature (°C)',
+                    },
+                },
+            },
+        };
+    }
+
+    return (
+        <>
+            {type === 'current' &&
+                <div className={`${cardBackground} text-center p-6 rounded-xl shadow-lg h-full w-full flex flex-col justify-between items-center`}>
+                    {/* Weather Info Section */}
+                    <div className="text-center">
+                        <h2 className={`text-2xl font-semibold ${textColor}`}>
+                            Current Weather in {locationData.name}, {locationData.country}
+                        </h2>
+                        <p className="text-md text-gray-300">
+                            Date and Time: {locationData.localtime}
+                        </p>
+                        <p className="flex items-center justify-center mt-2">
+                            {isDay ? <FaSun size={24} className={iconColor} /> : <FaMoon size={24} className={iconColor} />}
+                            <span className="ml-2 text-xl">{data.condition.text}</span>
+                        </p>
+                        <img
+                            src={data.condition.icon}
+                            alt={data.condition.text}
+                            className="mx-auto mt-3 w-16 h-16"
+                        />
+                    </div>
+
+                    {/* Current Temperature and Additional Info */}
+                    <div className="text-center mt-4">
+                        <p className={`text-5xl font-bold ${textColor}`}>{data.temp_c}°C</p>
+                        <p className="text-gray-300">Feels like {data.feelslike_c}°C</p>
+                        <div className="mt-4 flex justify-center space-x-6 text-lg">
+                            <div className="flex items-center">
+                                <FaWind size={20} className="text-gray-400 mr-1" />
+                                <p>{data.wind_kph} km/h</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FaTint size={20} className="text-blue-300 mr-1" />
+                                <p>{data.humidity}%</p>
+                            </div>
+                            <div className="flex items-center">
+                                <FaCloud size={20} className="text-gray-400 mr-1" />
+                                <p>{data.cloud}% Cloud</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Extra Info (Pressure, Visibility, UV Index) */}
+                    <div className="text-center mt-6 space-y-2">
+                        <p className="text-gray-300">Pressure: {data.pressure_mb} mb</p>
+                        <p className="text-gray-300">Visibility: {data.vis_km} km</p>
+                        <p className="text-gray-300">UV Index: {data.uv}</p>
+                    </div>
+                </div>
+            }
+
+            {/* Forecast Chart Section*/}
+            {type === 'forecast' && (
+                <div className="mt-6 w-full w-[50%]  h-[350px] bg-white rounded-xl justify-self-center">
+                    <Line
+                        data={chartData}
+                        options={{
+                            ...chartOptions,
+                            responsive: true,  // Ensure responsiveness
+                            maintainAspectRatio: false,  // Allow custom height for the chart
+                        }}
+                    />
+                </div>
+            )}
+
+        </>
+
+
+    );
+};
+
+export default WeatherCard;
